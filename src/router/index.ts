d48c8e8fc8e407a3d37d65ab94d1b9fc711be6ff
yangxiong <yx17835697335@163.com>
2021-03-05 17:03:17 +0800

    增加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '@/views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statisics from '@/views/Statisics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -23,6 +24,10 @@ const routes: Array<RouteConfig> = [
   {
     path:'/statistics',
     component: Statisics
+  },
+  {
+    path:'*',
+    component: NotFound
   }
 
 ];
